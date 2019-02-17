25e747113a1d7dcc30cbc011636f7092c101340c
Unknown <riderjensen@gmail.com>
2019-02-17 14:38:31 -0700

    Passing props down to a dynamic burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import classes from './Burger.css';
 import BurgerIngrdient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients).map(item => {
+		return [...Array(props.ingredients[item])].map((_, i) => {
+			return <BurgerIngrdient key={item + i} type={item} />
+		})
+	});
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngrdient type="bread-top" />
-			<BurgerIngrdient type="cheese" />
-			<BurgerIngrdient type="meat" />
+			{transformedIngredients}
 			<BurgerIngrdient type="bread-bottom" />
 		</div>
 	);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Auxil from '../../hoc/auxil';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 1,
+			cheese: 2,
+			meat: 2,
+		}
+	}
+
 	render() {
 		return (
 			<Auxil>
-				<Burger />
+				<Burger ingredients={this.state.ingredients} />
 				<div>Build Controls</div>
 			</Auxil>
 		);
