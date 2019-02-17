47b4418d7265b2a7820b80ea7362cddbe5ee3fcf
Unknown <riderjensen@gmail.com>
2019-02-17 15:02:03 -0700

    Conditional render add statement inbetween buns

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,11 +8,13 @@ const burger = (props) => {
 		return [...Array(props.ingredients[item])].map((_, i) => {
 			return <BurgerIngrdient key={item + i} type={item} />
 		})
-	});
+	}).reduce((arr, el) => {
+		return arr.concat(el)
+	}, []);
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngrdient type="bread-top" />
-			{transformedIngredients}
+			{transformedIngredients.length <= 0 ? <p>Please add some items</p> : transformedIngredients}
 			<BurgerIngrdient type="bread-bottom" />
 		</div>
 	);
