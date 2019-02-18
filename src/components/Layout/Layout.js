9bda38fcd223023ce77e49ba0b7446764eceea21
Unknown <riderjensen@gmail.com>
2019-02-18 10:15:55 -0700

    Import toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Auxil from '../../hoc/auxil';
 import classes from './Layout.css';
-
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 const layout = (props) => (
 	<Auxil>
-		<div>Toolbar, Sidedrawer, Backdrop</div>
+		<Toolbar />
 		<main className={classes.Content}>
 			{props.children}
 		</main>
