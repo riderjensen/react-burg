4be9fb8ff285ad2fb171236241d4761933eb212a
Unknown <riderjensen@gmail.com>
2019-02-18 11:14:38 -0700

    Performance updates for order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxil from '../../../hoc/auxil';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients).map(item => {
-		return <li key={item}><span style={{ textTransform: 'capitalize' }}>{item}</span>: {props.ingredients[item]}</li>
-	})
-	return (
-		<Auxil>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p>Total Price: <strong>{props.totalPrice.toFixed(2)}</strong></p>
-			<p>Continue to checkout?</p>
-			<Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-			<Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-		</Auxil>
-	);
+class OrderSummary extends Component {
+	render() {
+		const ingredientSummary = Object.keys(this.props.ingredients).map(item => {
+			return <li key={item}><span style={{ textTransform: 'capitalize' }}>{item}</span>: {this.props.ingredients[item]}</li>
+		})
+		return (
+			<Auxil>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p>Total Price: <strong>{this.props.totalPrice.toFixed(2)}</strong></p>
+				<p>Continue to checkout?</p>
+				<Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+			</Auxil>
+		);
+	}
 }
+
 export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Auxil from '../../../hoc/auxil';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-	return (
-		<Auxil>
-			<Backdrop show={props.show} clicked={props.modalClosed} />
-			<div
-				className={classes.Modal}
-				style={{
-					transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-					opacity: props.show ? '1' : '0'
+class Modal extends Component {
 
-				}}
-			>
-				{props.children}
-			</div>
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show
+	}
 
-		</Auxil>
+	render() {
+		return (
+			<Auxil>
+				<Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+				<div
+					className={classes.Modal}
+					style={{
+						transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+						opacity: this.props.show ? '1' : '0'
 
-	);
+					}}
+				>
+					{this.props.children}
+				</div>
+
+			</Auxil>
+
+		);
+	}
 }
 export default Modal;
