3844fa1da0fa3800700472b3be2f8862d07ac004
Unknown <riderjensen@gmail.com>
2019-02-18 10:29:52 -0700

    Side drawer creation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Auxil from '../../hoc/auxil';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 const layout = (props) => (
 	<Auxil>
 		<Toolbar />
+		<SideDrawer />
 		<main className={classes.Content}>
 			{props.children}
 		</main>
