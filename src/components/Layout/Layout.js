d64fbd2a5df97f048d0c0b64f58b9111c8998e3c
Unknown <riderjensen@gmail.com>
2019-02-18 10:53:56 -0700

    SIde drawer disappearing

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxil from '../../hoc/auxil';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-	<Auxil>
-		<Toolbar />
-		<SideDrawer />
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</Auxil>
-);
+class Layout extends Component {
+	state = {
+		showSideDrawer: true
+	}
+	sideDrawerClosedHandler = () => {
+		this.setState({
+			showSideDrawer: false
+		})
+	}
 
-export default layout;
+	render() {
+		return <Auxil>
+			<Toolbar />
+			<SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+			<main className={classes.Content}>
+				{this.props.children}
+			</main>
+		</Auxil>
+	}
+}
+
+export default Layout;
