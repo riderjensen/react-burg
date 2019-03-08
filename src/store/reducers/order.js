ab574faa7c36683004666432d564ec6e6fb15dc7
Rider Jensen <riderjensen@gmail.com>
2019-03-08 08:45:26 -0700

    update reducers with utility function

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,5 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
 
+import { updateObject } from '../utility';
+
 const initialState = {
 	orders: [],
 	loading: false,
@@ -9,47 +11,40 @@ const initialState = {
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.PURCHASE_INIT:
-			return {
-				...state,
+			return updateObject(state, {
 				purchased: false
-			}
+			})
 		case actionTypes.PURCHASE_BURGER_START:
-			return {
-				...state,
+			return updateObject(state, {
 				loading: true
-			}
+			})
 		case actionTypes.PURCHASE_BURGER_SUCCESS:
-			const newOrder = {
-				...action.orderData,
+
+			const newOrder = updateObject(action.orderData, {
 				id: action.orderId
-			}
-			return {
-				...state,
+			})
+			return updateObject(state, {
 				loading: false,
 				orders: state.orders.concat(newOrder),
 				purchased: true
-			}
+			})
 		case actionTypes.PURCHASE_BURGER_FAIL:
-			return {
-				...state,
-				loading: false,
-			}
+			return updateObject(state, {
+				loading: false
+			})
 		case actionTypes.FETCH_ORDERS_START:
-			return {
-				...state,
+			return updateObject(state, {
 				loading: true
-			}
+			})
 		case actionTypes.FETCH_ORDERS_SUCCESS:
-			return {
-				...state,
+			return updateObject(state, {
 				orders: action.orders,
 				loading: false
-			}
+			})
 		case actionTypes.FETCH_ORDERS_FAILED:
-			return {
-				...state,
+			return updateObject(state, {
 				loading: false
-			}
+			})
 		default:
 			return state;
 	}
