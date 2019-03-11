c146aff07fd61989ab4f41fe1f9dad7b6727152b
Rider Jensen <riderjensen@gmail.com>
2019-03-11 12:07:44 -0600

    Add Auth route

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,7 @@ const NavigationItems = (props) =>
 		<ul className={classes.NavigationItems}>
 			<NavigationItem link="/">Burger Builder</NavigationItem>
 			<NavigationItem link="/orders">Orders</NavigationItem>
+			<NavigationItem link="/auth">Authentication</NavigationItem>
 		</ul>
 	);
 
