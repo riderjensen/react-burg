9fbd436e7deaf4a1a8148df443d73a32c38c0c7a
Rider Jensen <riderjensen@gmail.com>
2019-03-12 14:26:29 -0600

    Set auth up for signing in

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,6 +37,7 @@ class BurgerBuilder extends Component {
 				purchasing: true
 			})
 		} else {
+			this.props.onSetAuthRedirectPath('/checkout')
 			this.props.history.push('/auth');
 		}
 
@@ -112,7 +113,8 @@ const mapDispatchToProps = dispatch => {
 		onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
 		onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
 		onInitIngredients: () => dispatch(actions.initIngredients()),
-		onInitPurchase: () => dispatch(actions.purchaseInit())
+		onInitPurchase: () => dispatch(actions.purchaseInit()),
+		onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
 	};
 }
 
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -5,6 +5,7 @@ const initState = {
 	userId: null,
 	error: null,
 	loading: false,
+	authRedirectPath: '/',
 };
 
 const authStart = (state, action) => {
@@ -32,12 +33,17 @@ const authLogOut = (state, action) => {
 	return updateObject(state, { idToken: null, userId: null })
 }
 
+const setAuthRedirectPath = (state, action) => {
+	return updateObject(state, { authRedirectPath: action.path })
+}
+
 const reducer = (state = initState, action) => {
 	switch (action.type) {
 		case actionTypes.AUTH_START: return authStart(state, action)
 		case actionTypes.AUTH_SUCCESS: return authSuccess(state, action)
 		case actionTypes.AUTH_FAIL: return authFail(state, action)
 		case actionTypes.AUTH_LOGOUT: return authLogOut(state, action)
+		case actionTypes.SET_AUTH_REDIRECT_PATH: return setAuthRedirectPath(state, action)
 		default:
 			return state;
 	}
