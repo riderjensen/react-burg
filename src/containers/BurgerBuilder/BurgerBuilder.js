f91a4ef81692b1dc9b9b63721e2d4e90533c96fc
Rider Jensen <riderjensen@gmail.com>
2019-03-28 09:19:20 -0600

    Add new test file for burger builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 import * as actions from '../../store/actions/index';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 	state = {
 		purchasing: false,
 	}
